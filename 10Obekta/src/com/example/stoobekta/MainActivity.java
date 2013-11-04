e215600db3ccbf1748e87eb82f85d1ccca6897df
Spas Tyurkedzhiev <harda@abv.bg>
2013-11-04 10:42:37 +0200

    Managed to get android id.All other unique identification methods dont work in emulator.Changed the emulator to genymotion(fuck you adt)

--- 10Obekta/src/com/example/stoobekta/MainActivity.java
+++ 10Obekta/src/com/example/stoobekta/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.stoobekta;
 
+import java.util.UUID;
+
 import android.os.Bundle;
+import android.provider.Settings.Secure;
 import android.app.Activity;
+import android.content.Context;
+import android.telephony.TelephonyManager;
 import android.view.Menu;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +16,8 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String id=getAndroidId(getApplicationContext());
+        Toast.makeText(this, id, Toast.LENGTH_LONG).show();
     }
 
 
@@ -18,6 +26,14 @@ public class MainActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+    
+    public static String getAndroidId(Context context)
+    {
+        String androidId =Secure.getString(context.getContentResolver(),
+        		Secure.ANDROID_ID);
+        return androidId;
+ 
     }
     
 }
